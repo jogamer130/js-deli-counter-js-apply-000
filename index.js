40986dad745784b39fb0bce1af02a84ee0c5902d
Derek Williams <jogamer130@gmail.com>
2020-01-29 21:27:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,14 +29,9 @@ function currentLine (line){
   if (line.length === 0){
     return "The line is currently empty."
   }
-<<<<<<< HEAD
    var x = [];
    for (var i=0; i<line.length; i++) {
    x.push(i+1 + ". " + line[i])
 }
   return "The line is currently: " + x.join(", ");
-=======
-  
-  return `The line is currently: 1. ${line[0]}, 2. ${line[1]}, 3. ${line[2]} `
->>>>>>> 67a2a0fcde32c981ed3124483d2b13c1999e8a68
 }
